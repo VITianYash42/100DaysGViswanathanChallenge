bdab360d117d9b8f1d7bac63a3b7c054d9d63d35
Yash Singhal <yash.24bce10839@vitbhopal.ac.in>
2026-07-25 21:52:20 +0530

    1829A Completion

---
+++ Solutions/Day08.java
@@ -0,0 +1,30 @@
+                                                   // 1829A - Love Story //
+
+package Solutions;
+import java.util.*;
+
+public class Day08 {
+    public static void main (String []args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        String s2 = "codeforces";
+        
+        while (t -- > 0) {
+            String s1 = sc.next();
+            int counter = 0;
+            
+            for (int i = 0; i < s1.length(); i++) {
+                char c1 = s1.charAt(i);
+                char c2 = s2.charAt(i);
+                
+                if (c1 != c2) {
+                    counter += 1;
+                }
+            }
+            System.out.println(counter);
+        }
+        sc.close();
+     }
+}
