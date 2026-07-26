a67fbbd14e9420b68f67144cfcdc333a88a6630f
Yash Singhal <yash.24bce10839@vitbhopal.ac.in>
2026-07-26 21:31:42 +0530

    1560A Completion

---
+++ Solutions/Day09.java
@@ -0,0 +1,30 @@
+                                                      // 1560A - Dislikes Of Threes //
+
+package Solutions;
+import java.util.*;
+
+public class Day09 {
+    public static void main(String []args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        
+        for (int i = 0; i<t; i++) {
+            int k = sc.nextInt();
+            
+            int currentNum = 0;
+            int LikeCount = 0;
+            
+            while (LikeCount < k) {
+                currentNum += 1;
+                
+                if (currentNum % 3 != 0 && currentNum % 10 != 3) {
+                    LikeCount += 1;
+                }
+            }
+            System.out.println(currentNum);
+        }
+        sc.close();
+    }
+}
