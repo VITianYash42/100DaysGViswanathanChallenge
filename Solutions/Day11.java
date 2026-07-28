f22d80e650e7d2e310b8d5906ffb85f68feb8d69
Yash Singhal <yash.24bce10839@vitbhopal.ac.in>
2026-07-28 18:14:37 +0530

    1850A Completion

---
+++ Solutions/Day11.java
@@ -0,0 +1,27 @@
+                                                  // 1850A - To My Critics //
+
+package Solutions;
+import java.util.*;
+
+public class Day11 {
+    public static void main(String []args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        
+        for (int i = 1; i<= t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            
+            if (a + b >= 10 || a + c >= 10 || b + c >= 10) {
+                System.out.println("YES");
+            }
+            else {
+                System.out.println("NO");
+            }
+        }
+        sc.close();
+    }
+}
