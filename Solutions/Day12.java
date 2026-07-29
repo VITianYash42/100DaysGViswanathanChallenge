c96fac0fefe3fd1338949dd826cde19dcda707d5
Yash Singhal <yash.24bce10839@vitbhopal.ac.in>
2026-07-29 21:42:34 +0530

    1807A Completion

---
+++ Solutions/Day12.java
@@ -0,0 +1,27 @@
+                                                      // 1807A - Plus or Minus //
+
+package Solutions;
+import java.util.*;
+
+public class Day12 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        
+        for (int i =1; i<=t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            
+            if (a + b == c) {
+                System.out.println("+");
+            }
+            else if (a - b == c) {
+                System.out.println("-");
+            }
+        }
+        sc.close();
+    }
+}
