250fe55d559a86c033e7c197c9b3ae422867c8bd
Yash Singhal <yash.24bce10839@vitbhopal.ac.in>
2026-07-31 21:26:12 +0530

    1692A Completion

---
+++ Solutions/Day14.java
@@ -0,0 +1,32 @@
+                                                       // 1692A - Marathon //
+
+package Solutions;
+import java.util.*;
+
+public class Day14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        
+        for (int i = 1; i<=t; i++) {
+            int counter = 0;
+            int a  = sc.nextInt();
+            int b  = sc.nextInt();
+            int c  = sc.nextInt();
+            int d  = sc.nextInt();
+            
+            if (a < b) {
+                counter++;
+            }
+            if (a < c) {
+                counter++;
+            }
+            if (a < d) {
+                counter++;
+            }
+            System.out.println(counter);
+        }
+        sc.close();
+    }
+}
