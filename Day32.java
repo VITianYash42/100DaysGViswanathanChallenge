fdf2ddc250b1902ac4ae88f49d2642a4e7ac18fc
Yash Singhal <yash.24bce10839@vitbhopal.ac.in>
2026-08-18 18:33:11 +0530

    2167A Completion

---
+++ Day32.java
@@ -0,0 +1,25 @@
+                                                  // 2167A - Square? //
+
+import java.util.*;
+
+public class Day32 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+            
+            if (a == b && a== c && a== d) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+        sc.close();
+    }
+}
