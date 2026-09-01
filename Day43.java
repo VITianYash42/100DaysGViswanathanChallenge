45525b3006de4e5bd607f073dda60a0605239b7c
Yash Singhal <yash.24bce10839@vitbhopal.ac.in>
2026-09-01 23:47:37 +0530

    677A Completion

---
+++ Day43.java
@@ -0,0 +1,26 @@
+                                                       // 677A - Vanya and Fence //
+
+import java.util.Scanner;
+
+public class Day43 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int h = sc.nextInt();
+        int totalWidth = 0;
+
+        for (int i = 0; i<n; i++) {
+            int height = sc.nextInt();
+            
+            if (height > h) {
+                totalWidth += 2;
+            } 
+            else {
+                totalWidth += 1;
+            }
+        }
+        System.out.println(totalWidth);
+        sc.close();
+    }
+}
