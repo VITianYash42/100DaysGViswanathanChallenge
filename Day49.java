7a948b3093fcdde3dd285daf56d0384494197bfb
Yash Singhal <yash.24bce10839@vitbhopal.ac.in>
2026-09-10 18:46:05 +0530

    469A Completion

---
+++ Day49.java
@@ -0,0 +1,31 @@
+                                            // 469A - I wanna be the Guy //
+
+import java.util.*;
+
+public class Day49 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        boolean[] levels = new boolean[n +1];
+        int p = sc.nextInt();
+        
+        for (int i = 0; i<p; i++) {
+            levels[sc.nextInt()] = true;
+        }
+
+        int q = sc.nextInt();
+        
+        for (int i = 0; i<q; i++) {
+            levels[sc.nextInt()] = true;
+        }
+
+        for (int i = 1; i <=n; i++) {
+            if (!levels[i]) {
+                System.out.println("Oh, my keyboard!");
+                return;
+            }
+        }
+        System.out.println("I become the guy.");
+    }
+}
